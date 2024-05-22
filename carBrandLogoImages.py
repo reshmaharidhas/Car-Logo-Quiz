88fbdbc77250ea6aa1400a39b7f9c6a33854d079
Reshma <37250413+reshmaharidhas@users.noreply.github.com>
2024-05-22 17:24:21 +0530

    Add files via upload

---
+++ carBrandLogoImages.py
@@ -0,0 +1,116 @@
+import tkinter
+
+def loadAudi():
+    return tkinter.PhotoImage(file="assets/images/logos/audi-1.png")
+
+def load_ferrari():
+    return tkinter.PhotoImage(file="assets/images/logos/ferrari.png")
+
+def loadLamborghini():
+    return tkinter.PhotoImage(file="assets/images/logos/lamborghini.png")
+def load_mitsubishi():
+    return tkinter.PhotoImage(file="assets/images/logos/mitsubishi.png")
+def load_subaru():
+    return tkinter.PhotoImage(file="assets/images/logos/subaru.png")
+def load_ligier():
+    return tkinter.PhotoImage(file="assets/images/logos/ligier.png")
+def load_mercedes():
+    return tkinter.PhotoImage(file="assets/images/logos/mercedes.png")
+def load_apollo_automobil():
+    return tkinter.PhotoImage(file="assets/images/logos/apollo-automobil.png")
+def load_volkswagen():
+    return tkinter.PhotoImage(file="assets/images/logos/volkswagen.png")
+def load_mahindra():
+    return tkinter.PhotoImage(file="assets/images/logos/mahindra.png")
+def load_maybach():
+    return tkinter.PhotoImage(file="assets/images/logos/maybach.png")
+def load_tesla():
+    return tkinter.PhotoImage(file="assets/images/logos/tesla.png")
+def load_porsche():
+    return tkinter.PhotoImage(file="assets/images/logos/porsche.png")
+def load_pontiac():
+    return tkinter.PhotoImage(file="assets/images/logos/pontiac.png")
+def load_peugeot():
+    return tkinter.PhotoImage(file="assets/images/logos/peugeot.png")
+def load_nissan():
+    return tkinter.PhotoImage(file="assets/images/logos/nissan.png")
+def load_mazda():
+    return tkinter.PhotoImage(file="assets/images/logos/mazda.png")
+def load_volvo():
+    return tkinter.PhotoImage(file="assets/images/logos/volvo.png")
+def load_lincoln():
+    return tkinter.PhotoImage(file="assets/images/logos/lincoln.png")
+def load_maserati():
+    return tkinter.PhotoImage(file="assets/images/logos/maserati.png")
+def load_rolls_royce():
+    return tkinter.PhotoImage(file="assets/images/logos/Rolls-Royce.png")
+def load_hyundai():
+    return tkinter.PhotoImage(file="assets/images/logos/hyundai.png")
+def load_honda():
+    return tkinter.PhotoImage(file="assets/images/logos/honda.png")
+def load_bmw():
+    return tkinter.PhotoImage(file="assets/images/logos/bmw.png")
+def load_bentley():
+    return tkinter.PhotoImage(file="assets/images/logos/bentley.png")
+def load_bugatti():
+    return tkinter.PhotoImage(file="assets/images/logos/bugatti.png")
+def load_yamaha():
+    return tkinter.PhotoImage(file="assets/images/logos/yamaha.png")
+def load_suzuki():
+    return tkinter.PhotoImage(file="assets/images/logos/suzuki.png")
+def load_astonmartin():
+    return tkinter.PhotoImage(file="assets/images/logos/astonmartin.png")
+def load_acura():
+    return tkinter.PhotoImage(file="assets/images/logos/acura.png")
+def load_ford():
+    return tkinter.PhotoImage(file="assets/images/logos/ford.png")
+def load_byd():
+    return tkinter.PhotoImage(file="assets/images/logos/byd.png")
+def load_cadillac():
+    return tkinter.PhotoImage(file="assets/images/logos/cadillac.png")
+def load_chevrolet():
+    return tkinter.PhotoImage(file="assets/images/logos/chevrolet.png")
+def load_citroen():
+    return tkinter.PhotoImage(file="assets/images/logos/citroen.png")
+def load_fiat():
+    return tkinter.PhotoImage(file="assets/images/logos/fiat.png")
+def load_renault():
+    return tkinter.PhotoImage(file="assets/images/logos/renault.png")
+def load_kia():
+    return tkinter.PhotoImage(file="assets/images/logos/kia.png")
+def load_jeep():
+    return tkinter.PhotoImage(file="assets/images/logos/jeep.png")
+def load_landrover():
+    return tkinter.PhotoImage(file="assets/images/logos/landrover.png")
+def load_lexus():
+    return tkinter.PhotoImage(file="assets/images/logos/lexus.png")
+def load_toyota():
+    return tkinter.PhotoImage(file="assets/images/logos/toyota.png")
+def load_alfa():
+    return tkinter.PhotoImage(file="assets/images/logos/alfa.png")
+def load_opel():
+    return tkinter.PhotoImage(file="assets/images/logos/opel.png")
+def load_skoda():
+    return tkinter.PhotoImage(file="assets/images/logos/skoda.png")
+def load_chrysler():
+    return tkinter.PhotoImage(file="assets/images/logos/chrysler.png")
+def load_mclaren():
+    return tkinter.PhotoImage(file="assets/images/logos/mclaren.png")
+def load_saab():
+    return tkinter.PhotoImage(file="assets/images/logos/saab.png")
+def load_lotus():
+    return tkinter.PhotoImage(file="assets/images/logos/lotus.png")
+def load_general_motors():
+    return tkinter.PhotoImage(file="assets/images/logos/general_motors.png")
+def load_buick():
+    return tkinter.PhotoImage(file="assets/images/logos/buick.png")
+def load_koenigsegg():
+    return tkinter.PhotoImage(file="assets/images/logos/koenigsegg.png")
+def load_dacia():
+    return tkinter.PhotoImage(file="assets/images/logos/dacia.png")
+def load_geely():
+    return tkinter.PhotoImage(file="assets/images/logos/geely.png")
+def load_rossion():
+    return tkinter.PhotoImage(file="assets/images/logos/rossion.png")
+def load_dodge():
+    return tkinter.PhotoImage(file="assets/images/logos/dodge.png")
